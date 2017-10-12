91788da396a69485acbb7f8709be9b017a3b571e
orache arthur <arthur.orache@gmail.com>
2017-10-12 13:42:14 +0300

    Add Badrequest error handler

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,17 +4,36 @@ from app.models import db, User, BlacklistToken
 from flask_httpauth import HTTPBasicAuth
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 import jwt
 import json
 import datetime
 
-
 auth = Blueprint('auth', __name__)
 
 
+def require_fields(*fields):
+    def decorate(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            for field in fields:
+                if not request.get_json(field, None):
+                    return jsonify({
+                        "error_msg": "Please fill all fields"
+                    })
+
+            return func(*args, **kwargs)
+
+        return wrap
+    return decorate
+
+
 @app.route('/auth/register', methods=['POST'])
+@require_fields('name', 'email', 'password')
 def register():
     data = request.get_json()
+    if not data['email'] or not data['password'] or not data['name']:
+        return make_response(jsonify({"message": "All fields are required"})), 403
     user = User.query.filter_by(email=data['email'],).first()
     if not user:
         new_user = User(name=data['name'], email=data['email'], password=data['password'])
@@ -34,20 +53,21 @@ def register():
 
 
 @app.route('/auth/login', methods=['POST'])
+@require_fields('email', 'password')
 def login():
-    #auth = request.authorization
-    #if not auth or not auth.username or not auth.password:
-        #return make_response(jsonify('Could not verify', 403, {'WWW-Authenticate': 'Basic realm="Login required!"'}))
-    data = request.get_json()
-    user = User.query.filter_by(email=data['email']).first()
 
+    data = request.get_json()
+
+    if not data['email'] or not data['password']:
+        return make_response(jsonify({"message": "All fields are required"})), 403
+    user = User.query.filter_by(email=data['email']).first()
     if not user:
         responseObject = {
             'status': 'fail',
             'message': 'User does not exist.'
         }
         return make_response(jsonify(responseObject)), 401
-        
+
     if check_password_hash(user.password, data['password']):
         payload = {
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=20),
@@ -67,9 +87,9 @@ def login():
         }
 
         return make_response(jsonify(response)), 200
+
     else:
-        responseObject = {
-            'status': 'fail',
-            'message': 'Try again'
+        response = {
+            'message': 'Failed to login'
         }
-        return make_response(jsonify(responseObject)), 401
+        return make_response(jsonify(response)), 401
