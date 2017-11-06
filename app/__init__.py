cf9ff1509c4ac1b03e6a4435b60587d9f5dcd412
orache arthur <arthur.orache@gmail.com>
2017-11-06 18:03:52 +0300

    Add docstring

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from app.errors import bad_request, internal_server_error
 
 
 app = Flask(__name__)
+
 app.register_error_handler(400, bad_request)
 app.register_error_handler(500, internal_server_error)
+
 from app import views
