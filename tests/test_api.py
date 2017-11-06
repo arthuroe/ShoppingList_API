a37d8eeedaa2397dfaa19cd43f3a31537f745c79
orache arthur <arthur.orache@gmail.com>
2017-11-06 18:54:45 +0300

    Refactor tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -33,7 +33,7 @@ class AuthTestCase(unittest.TestCase):
                      'email': email,
                      'password': password
                      }
-        return self.app.post('/v1/auth/register', data=json.dumps(user_data), content_type='application/json')
+        return self.app.post('/api/v1/auth/register', data=json.dumps(user_data), content_type='application/json')
 
     def login_user(self, email="test@gmail.com", password="test1234"):
         """This helper method helps log in a test user."""
@@ -41,7 +41,7 @@ class AuthTestCase(unittest.TestCase):
             'email': email,
             'password': password
         }
-        return self.app.post('/v1/auth/login', data=json.dumps(user_data), content_type='application/json')
+        return self.app.post('/api/v1/auth/login', data=json.dumps(user_data), content_type='application/json')
 
     def test_create_shopping_list_for_registered_user(self):
         """Test create shopping list for user"""
@@ -49,14 +49,14 @@ class AuthTestCase(unittest.TestCase):
             self.register_user()
             result = self.login_user()
             access_token = json.loads(result.data.decode())['access_token']
-            result = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+            result = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                                    headers={'Content-Type': 'application/json', 'access-token': access_token})
             self.assertEqual(result.status_code, 201)
             self.assertIn('list added', str(result.data))
 
     def test_create_shopping_list_for_unregistered_user(self):
         """Test create shopping list for unregisterd user"""
-        result = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        result = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                                headers={'Content-Type': 'application/json', 'access-token': ''})
         self.assertEqual(result.status_code, 403)
         self.assertIn('Token is missing!', str(result.data))
@@ -67,12 +67,12 @@ class AuthTestCase(unittest.TestCase):
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
 
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists', headers={'Content-Type': 'application/json',
-                                                                'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists', headers={'Content-Type': 'application/json',
+                                                                    'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
 
     def test_search_shopping_lists(self):
@@ -81,12 +81,12 @@ class AuthTestCase(unittest.TestCase):
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
 
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists?q=test_list', headers={'Content-Type': 'application/json',
-                                                                            'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists?q=test_list', headers={'Content-Type': 'application/json',
+                                                                                'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
 
     def test_view_one_shopping_list(self):
@@ -95,12 +95,12 @@ class AuthTestCase(unittest.TestCase):
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
 
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
 
     def test_update_shopping_list(self):
@@ -108,15 +108,15 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
 
-        update_res = self.app.put('/v1/shoppinglists/1', data=json.dumps({"name": "new_name"}),
+        update_res = self.app.put('/api/v1/shoppinglists/1', data=json.dumps({"name": "new_name"}),
                                   headers={'Content-Type': 'application/json', 'access-token': access_token})
 
         self.assertEqual(update_res.status_code, 200)
@@ -126,15 +126,15 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
-        del_result = self.app.delete('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                     'access-token': access_token})
+        del_result = self.app.delete('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                         'access-token': access_token})
         self.assertEqual(del_result.status_code, 200)
 
     def test_add_item_to_shopping_list(self):
@@ -142,14 +142,14 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
-        item = self.app.post('/v1/shoppinglists/1/items', data=json.dumps(self.item),
+        item = self.app.post('/api/v1/shoppinglists/1/items', data=json.dumps(self.item),
                              headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(item.status_code, 201)
 
@@ -158,17 +158,17 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
-        item = self.app.post('/v1/shoppinglists/1/items', data=json.dumps(self.item),
+        item = self.app.post('/api/v1/shoppinglists/1/items', data=json.dumps(self.item),
                              headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(item.status_code, 201)
-        update_item = self.app.put('/v1/shoppinglists/1/items/1', data=json.dumps({"name": "new_name"}),
+        update_item = self.app.put('/api/v1/shoppinglists/1/items/1', data=json.dumps({"name": "new_name"}),
                                    headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(update_item.status_code, 200)
 
@@ -177,17 +177,17 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
-        item = self.app.post('/v1/shoppinglists/1/items', data=json.dumps(self.item),
+        item = self.app.post('/api/v1/shoppinglists/1/items', data=json.dumps(self.item),
                              headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(item.status_code, 201)
-        view_item = self.app.get('/v1/shoppinglists/1/items',
+        view_item = self.app.get('/api/v1/shoppinglists/1/items',
                                  headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(view_item.status_code, 200)
 
@@ -196,18 +196,18 @@ class AuthTestCase(unittest.TestCase):
         self.register_user()
         result = self.login_user()
         access_token = json.loads(result.data.decode())['access_token']
-        res = self.app.post('/v1/shoppinglists/', data=json.dumps(self.shopping_list),
+        res = self.app.post('/api/v1/shoppinglists/', data=json.dumps(self.shopping_list),
                             headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(res.status_code, 201)
 
-        get_result = self.app.get('/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
-                                                                  'access-token': access_token})
+        get_result = self.app.get('/api/v1/shoppinglists/1', headers={'Content-Type': 'application/json',
+                                                                      'access-token': access_token})
         self.assertEqual(get_result.status_code, 200)
-        item = self.app.post('/v1/shoppinglists/1/items', data=json.dumps(self.item),
+        item = self.app.post('/api/v1/shoppinglists/1/items', data=json.dumps(self.item),
                              headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(item.status_code, 201)
         delete_item = self.app.delete(
-            '/v1/shoppinglists/1/items/1', headers={'Content-Type': 'application/json', 'access-token': access_token})
+            '/api/v1/shoppinglists/1/items/1', headers={'Content-Type': 'application/json', 'access-token': access_token})
         self.assertEqual(delete_item.status_code, 200)
 
 
